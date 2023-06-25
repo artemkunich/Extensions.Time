18c7f02355962658a12363a6e3b00a0f376c2007
Artem Kunich <artem13081991@gmail.com>
2023-06-25 13:13:12 +0200

    make SystemTimeProvider internal

--- src/SystemTimeProvider.cs
+++ src/SystemTimeProvider.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Akunich.Extensions.Time;
 
-public sealed class SystemTimeProvider : ITimeProvider
+internal sealed class SystemTimeProvider : ITimeProvider
 {
     public DateTime GetLocalNow() => DateTime.Now;
 
